9ec8c59491904ab6b3d71c6d6ef11708d0bf130b
Lil-Prech <111912882+Lil-Prech@users.noreply.github.com>
2024-06-22 01:45:11 -1100

    Add files via upload

--- PROJECT/tailwind.config.js
+++ PROJECT/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index/**/*.{html,js}",
+    "./dashboard/**/*.{html,js}",
   ],
   prefix: "tw-",
   important: true,
